10ee69c9490189d2ce646284045babd6be9c0d08
Jodu555 <Jodu505@gmail.com>
2022-04-23 15:37:40 +0200

    Added the uuid and playername stuff

--- src/McPlayer.js
+++ src/McPlayer.js
@@ -1,12 +1,25 @@
 class McPlayer {
-    constructor(nameOrUUID) {
 
+    /**
+     * @param  {String} nameOrUUID can either be the minecraft player name or his UUID
+     */
+    constructor(nameOrUUID) {
+        console.log(nameOrUUID.length);
+        this.playername = null;
+        this.uuid = null;
+        if (nameOrUUID.length <= 16) {
+            this.playername = nameOrUUID;
+        } else {
+            this.uuid = nameOrUUID;
+        }
     }
     getName() {
-
+        if (this.playername != null)
+            return this.playername
     }
     getUUID() {
-
+        if (this.uuid != null)
+            return this.uuid
     }
     getSking() {
 
