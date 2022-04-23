cd66fdbdecb82584964056362d044d3125a33fb9
Jodu555 <Jodu505@gmail.com>
2022-04-23 19:20:44 +0200

    addiotions + fixes

--- test.js
+++ test.js
@@ -1,10 +1,23 @@
-const McPlayer = require("./src/McPlayer");
+// const McPlayer = require("./src/McPlayer");
 
-const playerName = new McPlayer('jodu555');
-const playerUUID = new McPlayer('076b1e9c37714e84b1c70638514aba2e');
-const playerUUID2 = new McPlayer('076b1e9c-3771-4e84-b1c7-0638514aba2e');
+// const playerName = new McPlayer('jodu555');
+// const playerUUID = new McPlayer('076b1e9c37714e84b1c70638514aba2e');
+// const playerUUID2 = new McPlayer('076b1e9c-3771-4e84-b1c7-0638514aba2e');
 
-// playerUUID.getName();
-// playerUUID2.getName()
+// // playerUUID.getName();
+// // playerUUID2.getName()
 
-playerUUID.getSkin();
+// playerUUID.getSkin();
+
+const sharp = require('sharp');
+
+
+//TODO: This works fine for my skin
+sharp('sk.png').extract({ width: 8, height: 8, left: 40, top: 8 }).toFile('out.png')
+    .then(function (new_file_info) {
+        console.log("Image cropped and saved");
+    })
+    .catch(function (err) {
+        console.log("An error occured");
+        console.log(err);
+    });
